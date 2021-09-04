2eeb7734867617944e1dfce64147067609e6e989
minkukjo <strike0115@naver.com>
2021-09-05 00:15:18 +0900

    make htmlStatement, htmlStatement unit test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -64,16 +64,34 @@ fun createStatementData(invoice: Invoice, plays: Map<String, Play>): StatementDa
     }
 }
 
+fun htmlStatement(invoice: Invoice, plays: Map<String, Play>): String {
+    return renderHtml(createStatementData(invoice, plays))
+}
+
+fun usd(number: Int): String {
+    return NumberFormat.getCurrencyInstance(Locale.US).format(number / 100)
+}
+
+fun renderHtml(data: StatementData): String {
+    var result = "<h1>청구 내역 (고객명: ${data.customer})</h1>\n"
+    result += "<table>\n"
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>"
+    for (performance in data.performances) {
+        result += "<tr><td>${performance.play?.name}</td><td>${performance.audience}석</td>"
+        result += "<td>${usd(performance.amount)}</td></tr>\n"
+    }
+    result += "</table>\n"
+    result += "<p>총액: <em>${usd(data.totalAmount)}</em></p>\n"
+    result += "<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>"
+    return result
+}
+
 fun statement(invoice: Invoice, plays: Map<String, Play>): String {
     return renderPlainText(createStatementData(invoice, plays))
 }
 
 fun renderPlainText(data: StatementData): String {
-    fun usd(number: Int): String {
-        return NumberFormat.getCurrencyInstance(Locale.US).format(number / 100)
-    }
-
-    var result = "청구 내역 (고객명: ${data.customer}\n"
+    var result = "청구 내역 (고객명: ${data.customer})\n"
 
     for (performance in data.performances) {
         // 청구 내역을 출력한다.
@@ -94,7 +112,6 @@ fun getInvoices(json: Json): List<Invoice> {
     return json.decodeFromString(invoicesJson)
 }
 
-
 fun main() {
     val json = Json {
         ignoreUnknownKeys = true
@@ -102,4 +119,5 @@ fun main() {
     val plays = getPlays(json)
     val invoices = getInvoices(json)
     println(statement(invoices[0], plays))
+    println(htmlStatement(invoices[0], plays))
 }
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -21,12 +21,39 @@ internal class MainKtTest {
 
         // then
         Assertions.assertEquals("""
-            청구 내역 (고객명: BigCo
+            청구 내역 (고객명: BigCo)
             Hamlet: ${'$'}650.00 55석
             As You Like It: ${'$'}580.00 35석
             Othello: ${'$'}500.00 40석
             총액: ${'$'}1,730.00
             적립 포인트: 47점
+            """.trimIndent(), result)
+    }
+
+    @Test
+    @DisplayName("htmlStatement 실행 결과 테스트")
+    fun testHtmlStatement() {
+        // given
+        val json = Json {
+            ignoreUnknownKeys = true
+        }
+        val plays = getPlays(json)
+        val invoices = getInvoices(json)
+
+        // when
+        val result = htmlStatement(invoices[0], plays)
+        println(result)
+
+        // then
+        Assertions.assertEquals("""
+            <h1>청구 내역 (고객명: BigCo)</h1>
+            <table>
+            <tr><th>연극</th><th>좌석 수</th><th>금액</th></tr><tr><td>Hamlet</td><td>55석</td><td>${'$'}650.00</td></tr>
+            <tr><td>As You Like It</td><td>35석</td><td>${'$'}580.00</td></tr>
+            <tr><td>Othello</td><td>40석</td><td>${'$'}500.00</td></tr>
+            </table>
+            <p>총액: <em>${'$'}1,730.00</em></p>
+            <p>적립 포인트: <em>47</em>점</p>
             """.trimIndent(), result)
     }
 }
