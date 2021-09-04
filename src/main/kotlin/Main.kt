8a92783763a82d518cd8b1c298a8272c79b73c73
minkukjo <strike0115@naver.com>
2021-09-04 21:16:26 +0900

    move functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,50 +5,47 @@ import java.text.NumberFormat
 import java.util.Locale
 import kotlin.math.floor
 
-fun amountFor(performance: Performance, play: Play): Int {
-    var result = 0
-    when (play.type) {
-        PlayType.TRAGEDY -> {
-            result = 40000;
-            if (performance.audience > 30) {
-                result += 1000 * (performance.audience - 30)
-            }
-        }
-        PlayType.COMEDY -> {
-            result = 30000;
-            if (performance.audience > 20) {
-                result += 10000 + 500 * (performance.audience - 20)
-            }
-            result += 300 * performance.audience
-        }
-        else -> throw Error("알 수 없는 장르: ${play.type}")
-    }
-    return result
-}
-
-
 fun statement(invoices: Invoice, plays: Map<String, Play>): String {
-    var totalAmount = 0
-    var volumeCredits = 0
-    var result = "청구 내역 (고객명: ${invoices.customer}\n"
-    val format = NumberFormat.getCurrencyInstance(Locale.US)
 
     fun playFor(performance: Performance): Play {
         return plays[performance.playId] ?: throw ClassNotFoundException("should be exist")
     }
 
-    for (performance in invoices.performances) {
-        val play = playFor(performance)
+    fun amountFor(performance: Performance): Int {
+        var result = 0
+        when (playFor(performance).type) {
+            PlayType.TRAGEDY -> {
+                result = 40000;
+                if (performance.audience > 30) {
+                    result += 1000 * (performance.audience - 30)
+                }
+            }
+            PlayType.COMEDY -> {
+                result = 30000;
+                if (performance.audience > 20) {
+                    result += 10000 + 500 * (performance.audience - 20)
+                }
+                result += 300 * performance.audience
+            }
+        }
+        return result
+    }
 
-        val thisAmount = amountFor(performance, play)
+    var totalAmount = 0
+    var volumeCredits = 0
+    var result = "청구 내역 (고객명: ${invoices.customer}\n"
+    val format = NumberFormat.getCurrencyInstance(Locale.US)
+
+    for (performance in invoices.performances) {
+        val thisAmount = amountFor(performance)
 
         // 포인트를 적립한다.
         volumeCredits += (performance.audience - 30).coerceAtLeast(0)
         // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if (PlayType.COMEDY == play.type) volumeCredits += floor((performance.audience / 5).toDouble()).toInt()
+        if (PlayType.COMEDY == playFor(performance).type) volumeCredits += floor((performance.audience / 5).toDouble()).toInt()
 
         // 청구 내역을 출력한다.
-        result += "${play.name}: ${format.format(thisAmount / 100)} ${performance.audience}석\n"
+        result += "${playFor(performance).name}: ${format.format(thisAmount / 100)} ${performance.audience}석\n"
         totalAmount += thisAmount
     }
     result += "총액: ${format.format(totalAmount / 100)}\n"
@@ -56,13 +53,19 @@ fun statement(invoices: Invoice, plays: Map<String, Play>): String {
     return result
 }
 
+fun getPlays(): Map<String, Play> {
+    val playsJson = File("./src/main/resources/plays.json").readText(Charsets.UTF_8)
+    return Json.decodeFromString(playsJson)
+}
+
+fun getInvoices(): List<Invoice> {
+    val invoicesJson = File("./src/main/resources/invoices.json").readText(Charsets.UTF_8)
+    return Json.decodeFromString(invoicesJson)
+}
+
 
 fun main() {
-    val playsJson = File("./src/main/resources/plays.json").readText(Charsets.UTF_8)
-    val plays = Json.decodeFromString<Map<String, Play>>(playsJson)
-
-    val invoicesJson = File("./src/main/resources/invoices.json").readText(Charsets.UTF_8)
-    val invoices = Json.decodeFromString<List<Invoice>>(invoicesJson)
-
+    val plays = getPlays()
+    val invoices = getInvoices()
     println(statement(invoices[0], plays))
 }
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,30 +1,21 @@
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import java.io.File
 
 internal class MainKtTest {
-    private lateinit var plays: Map<String, Play>
-
-    private lateinit var invoices: List<Invoice>
-
-    @BeforeEach
-    fun setUp() {
-        val playsJson = File("./src/main/resources/plays.json").readText(Charsets.UTF_8)
-        plays = Json.decodeFromString(playsJson)
-
-        val invoicesJson = File("./src/main/resources/invoices.json").readText(Charsets.UTF_8)
-        invoices = Json.decodeFromString(invoicesJson)
-    }
 
     @Test
     @DisplayName("statement 실행 결과 테스트")
     fun testStatement() {
+        // given
+        val plays = getPlays()
+        val invoices = getInvoices()
+
+        // when
         val result = statement(invoices[0], plays)
         println(result)
+
+        // then
         Assertions.assertEquals("""
             청구 내역 (고객명: BigCo
             Hamlet: ${'$'}650.00 55석
