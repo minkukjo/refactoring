dfaeb42544867ed5b93111c8b75e1c29396b8e23
minkukjo <strike0115@naver.com>
2021-10-12 00:58:26 +0900

    6-2 함수 인라인 하기 코드 개선

--- src/main/kotlin/application/ch6/InlineFunction.kt
+++ src/main/kotlin/application/ch6/InlineFunction.kt
@@ -1,19 +1,24 @@
 package application.ch6
 
+import domain.ch6.Customer
 import domain.ch6.Driver
 
 fun rating(driver: Driver): Int {
-    fun moreThanFiveLateDeliveries(driver: Driver): Boolean {
-        return driver.numberOfLateDeliveries > 5
-    }
-    return if (moreThanFiveLateDeliveries(driver)) {
+    return if (driver.numberOfLateDeliveries > 5) {
         2
     } else {
         1
     }
 }
 
+fun reportLines(customer: Customer): MutableMap<String, String> {
+    val lines = mutableMapOf<String, String>()
+    lines["name"] = customer.name
+    lines["location"] = customer.location
+    return lines
+}
+
 fun main() {
-    val rating = rating(Driver(numberOfLateDeliveries = 6))
-    println(rating)
+    val reportLines = reportLines(Customer(name = "harry", location = "mars"))
+    println(reportLines)
 }
---
+++ src/main/kotlin/domain/ch6/Customer.kt
@@ -0,0 +1,6 @@
+package domain.ch6
+
+class Customer(
+        val name: String,
+        val location: String
+)
