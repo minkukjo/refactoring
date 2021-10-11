f7b61cba41a6bec38239f318065430054c7ba083
minkukjo <strike0115@naver.com>
2021-10-12 01:14:49 +0900

    6-5 함수 선언의 매개변수 바꾸기

--- src/main/kotlin/application/ch6/ChangeFunctionDeclaration.kt
+++ src/main/kotlin/application/ch6/ChangeFunctionDeclaration.kt
@@ -1,5 +1,18 @@
 package application.ch6
 
-fun circum(radius: Double): Double {
+import domain.ch6.Customer
+
+// circumference 원의 둘레라는 뜻
+fun circumference(radius: Double): Double {
     return 2 * Math.PI * radius
+}
+
+val reservations: MutableList<Customer> = mutableListOf()
+
+fun addReservation(customer: Customer) {
+    fun zz_addReservation(customer: Customer, isPriority: Boolean) {
+        reservations.add(customer)
+    }
+
+    zz_addReservation(customer, false)
 }
