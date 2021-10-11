d618bbbd5a912ba984a07b52c2479b7fdb6d10ea
minkukjo <strike0115@naver.com>
2021-10-12 01:06:29 +0900

    6-3 변수 추출하여 리팩토링

--- src/main/kotlin/application/ch6/AbstractVariable.kt
+++ src/main/kotlin/application/ch6/AbstractVariable.kt
@@ -3,7 +3,9 @@ package application.ch6
 import domain.ch6.Order
 
 fun price(order: Order): Double {
-    return order.quantity * order.itemPrice - 0.coerceAtLeast(order.quantity - 500) * order.itemPrice * 0.05 +
-            (order.quantity * order.itemPrice * 0.1).coerceAtMost(100.0)
+    val basePrice = order.quantity * order.itemPrice
+    val quantityDiscount = 0.coerceAtLeast(order.quantity - 500) * order.itemPrice * 0.05
+    val shipping = (basePrice * 0.1).coerceAtMost(100.0)
+    return basePrice - quantityDiscount + shipping
 }
 
