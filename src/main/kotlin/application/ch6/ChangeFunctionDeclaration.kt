0ddcf812b4176d2e0204b4f983a0ca8aa02a6d08
minkukjo <strike0115@naver.com>
2021-10-12 01:21:08 +0900

    6-5 함수 매개변수를 속성으로 바꾸기

--- src/main/kotlin/application/ch6/ChangeFunctionDeclaration.kt
+++ src/main/kotlin/application/ch6/ChangeFunctionDeclaration.kt
@@ -1,18 +1,29 @@
 package application.ch6
 
 import domain.ch6.Customer
+import java.util.LinkedList
+import java.util.PriorityQueue
+import java.util.Queue
 
 // circumference 원의 둘레라는 뜻
 fun circumference(radius: Double): Double {
     return 2 * Math.PI * radius
 }
 
-val reservations: MutableList<Customer> = mutableListOf()
+val reservations: Queue<Customer> = LinkedList()
+val priorityReservations: PriorityQueue<Customer> = PriorityQueue()
 
-fun addReservation(customer: Customer) {
-    fun zz_addReservation(customer: Customer, isPriority: Boolean) {
+fun addReservation(customer: Customer, isPriority: Boolean) {
+    if (isPriority) {
+        priorityReservations.add(customer)
+    } else {
         reservations.add(customer)
     }
+}
 
-    zz_addReservation(customer, false)
+fun inNewEngland(customer: Customer): Boolean {
+    fun xxNEWinNewEngland(stateCode: String): Boolean {
+        return mutableListOf("MA", "CT", "ME", "VT", "NH", "RI").contains(stateCode)
+    }
+    return xxNEWinNewEngland(customer.location)
 }
