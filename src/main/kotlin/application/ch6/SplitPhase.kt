a5b0671b6c2aa24310835bd17c3910b911a9553e
minkukjo <strike0115@naver.com>
2021-10-15 18:22:41 +0900

    6-11 배송비 계산 함수 함수로 추출

--- src/main/kotlin/application/ch6/SplitPhase.kt
+++ src/main/kotlin/application/ch6/SplitPhase.kt
@@ -3,14 +3,19 @@ package application.ch6
 import domain.ch6.Product
 import domain.ch6.ShippingMethod
 
-fun priceOrder(product: Product, quantity: Int, shippingMethod: ShippingMethod): Int {
-    val basePrice = product.basePrice * quantity
-    val discount = (quantity - product.discountThreshold).coerceAtLeast(0)
+fun applyShipping(basePrice: Int, shippingMethod: ShippingMethod, quantity: Int, discount: Int): Int {
     val shippingPerCase = if (basePrice > shippingMethod.discountThreshold) {
         shippingMethod.discountedFee
     } else {
         shippingMethod.feePerCase
     }
     val shippingCost = quantity * shippingPerCase
-    return basePrice - discount + shippingCost
+    return basePrice - discount + shippingCost;
+}
+
+fun priceOrder(product: Product, quantity: Int, shippingMethod: ShippingMethod): Int {
+    val basePrice = product.basePrice * quantity
+    val discount = (quantity - product.discountThreshold).coerceAtLeast(0)
+    
+    return applyShipping(basePrice, shippingMethod, quantity, discount)
 }
