ed0b09f57da1983ed68d3f9a166260767287c308
minkukjo <strike0115@naver.com>
2021-10-15 18:26:37 +0900

    6-11 매개 변수 중간 클래스로 치환

--- src/main/kotlin/application/ch6/SplitPhase.kt
+++ src/main/kotlin/application/ch6/SplitPhase.kt
@@ -1,21 +1,22 @@
 package application.ch6
 
+import domain.ch6.PriceData
 import domain.ch6.Product
 import domain.ch6.ShippingMethod
 
-fun applyShipping(basePrice: Int, shippingMethod: ShippingMethod, quantity: Int, discount: Int): Int {
-    val shippingPerCase = if (basePrice > shippingMethod.discountThreshold) {
+fun applyShipping(priceData: PriceData, shippingMethod: ShippingMethod): Int {
+    val shippingPerCase = if (priceData.basePrice > shippingMethod.discountThreshold) {
         shippingMethod.discountedFee
     } else {
         shippingMethod.feePerCase
     }
-    val shippingCost = quantity * shippingPerCase
-    return basePrice - discount + shippingCost;
+    val shippingCost = priceData.quantity * shippingPerCase
+    return priceData.basePrice - priceData.discount + shippingCost;
 }
 
 fun priceOrder(product: Product, quantity: Int, shippingMethod: ShippingMethod): Int {
     val basePrice = product.basePrice * quantity
     val discount = (quantity - product.discountThreshold).coerceAtLeast(0)
-    
-    return applyShipping(basePrice, shippingMethod, quantity, discount)
+    val priceData = PriceData(basePrice, quantity, discount)
+    return applyShipping(priceData, shippingMethod)
 }
---
+++ src/main/kotlin/domain/ch6/PriceData.kt
@@ -0,0 +1,7 @@
+package domain.ch6
+
+class PriceData(
+        val basePrice: Int,
+        val quantity: Int,
+        val discount: Int
+)
