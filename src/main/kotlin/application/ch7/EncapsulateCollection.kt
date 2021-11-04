30b524dc8dccb38e91d8145777899b4864807f90
minkukjo <strike0115@naver.com>
2021-11-04 20:25:54 +0900

    컬렉션 캡슐화하기

--- src/main/kotlin/application/ch7/EncapsulateCollection.kt
+++ src/main/kotlin/application/ch7/EncapsulateCollection.kt
@@ -5,6 +5,10 @@ import domain.ch7.Person
 
 fun main() {
     val basicCourseNames = arrayOf("harry", "denis", "jane")
-    val aPerson = Person("test", arrayOf())
-    aPerson.courses = basicCourseNames.map { name -> Course(name, false) }.toTypedArray()
+    val aPerson = Person("test", mutableListOf())
+    // 캡슐화가 깨졌다.
+//    aPerson.courses = basicCourseNames.map { name -> Course(name, false) }.toTypedArray()
+
+    // aPerson의 책임은 자신이 관리하는 Course를 관리하는 것
+    basicCourseNames.forEach { name -> aPerson.addCourse(Course(name, false)) }
 }
--- src/main/kotlin/domain/ch7/Person.kt
+++ src/main/kotlin/domain/ch7/Person.kt
@@ -2,5 +2,15 @@ package domain.ch7
 
 class Person(
         val name: String,
-        var courses: Array<Course>
-)
+        val courses: MutableList<Course>
+) {
+    fun addCourse(aCourse: Course) {
+        this.courses.add(aCourse)
+    }
+
+    fun removeCourse(aCourse: Course, fnIfAbsent: () -> Unit) {
+        val index = courses.indexOf(aCourse)
+        if (index == -1) fnIfAbsent()
+        else courses.remove(aCourse)
+    }
+}
