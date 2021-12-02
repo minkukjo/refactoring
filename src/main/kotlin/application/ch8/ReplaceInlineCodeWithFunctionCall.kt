37ad40768d0d20f43d9e5637e60c47781c9bde46
minkukjo <strike0115@naver.com>
2021-12-02 23:09:23 +0900

    8.5 인라인 코드 함수로 변경

--- src/main/kotlin/application/ch8/ReplaceInlineCodeWithFunctionCall.kt
+++ src/main/kotlin/application/ch8/ReplaceInlineCodeWithFunctionCall.kt
@@ -3,11 +3,6 @@ package application.ch8
 // 8.5 인라인 코드를 함수 호출로 대체하자. (중복코드의 재활용)
 
 fun main() {
-    var appliesToMass = false
     val states = listOf("normal", "test", "MA")
-    for (s in states) {
-        if (s == "MA") {
-            appliesToMass = true
-        }
-    }
+    var appliesToMass = states.contains("MA")
 }
