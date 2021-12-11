fa9ab88423311c663f5f9be21e77b42ba992c313
minkukjo <strike0115@naver.com>
2021-12-11 21:04:09 +0900

    조건문 분해하기

--- src/main/kotlin/application/ch10/DecomposeConditional.kt
+++ src/main/kotlin/application/ch10/DecomposeConditional.kt
@@ -5,19 +5,31 @@ import java.time.LocalDateTime
 // 10.1 조건문 분해하기
 // 복잡한 조건문은 간결하게 함수로 추출
 
+val quantity = 0.8
+val summerRate = 0.08
+val regularRate = 0.10
 
-fun main() {
+fun summer(): Boolean {
     val date = LocalDateTime.now()
     val summerStart = LocalDateTime.now().plusMonths(6L)
     val summerEnd = LocalDateTime.now().plusMonths(9L)
+    return !date.isBefore(summerStart) && !date.isAfter(summerEnd)
+}
+
+fun summerCharge(): Double {
+    return quantity * summerRate
+}
+
+fun regularCharge(): Double {
+    return quantity * regularRate
+}
+
+fun main() {
     var charge = 0.0
-    val quantity = 0.8
-    val summerRate = 0.08
-    val regularRate = 0.10
-    charge = if (!date.isBefore(summerStart) && !date.isAfter(summerEnd)) {
-        quantity * summerRate
+    charge = if (summer()) {
+        summerCharge()
     } else {
-        quantity * regularRate
+        regularCharge()
     }
     println(charge)
 }
