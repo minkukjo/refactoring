b3bf2f511dfb5d48f5843f3ea610d783b3110e12
minkukjo <strike0115@naver.com>
2021-12-13 23:32:27 +0900

    Bird 클래스 내부로 조건문 이동

--- src/main/kotlin/application/ch10/ReplaceConditionalWithPolymorphism.kt
+++ src/main/kotlin/application/ch10/ReplaceConditionalWithPolymorphism.kt
@@ -16,31 +16,9 @@ fun speeds(birds: List<Bird>): Map<String, List<Int>> {
 }
 
 fun plumage(bird: Bird): String { // 깃털 상태
-    return when (bird.type) {
-        "유럽 제비" -> "보통이다"
-        "아프리카 제비" -> if (bird.numberOfCoconuts > 2) {
-            "지쳤다"
-        } else {
-            "보통이다"
-        }
-        "노르웨이 파랑 앵무" -> if (bird.voltage > 100) {
-            "그을렸다"
-        } else {
-            "예쁘다"
-        }
-        else -> "알 수 없다."
-    }
+    return Bird(bird).plumage()
 }
 
 fun airSpeedVelocity(bird: Bird): Int { // 깃털 상태
-    return when (bird.type) {
-        "유럽 제비" -> 35
-        "아프리카 제비" -> 35 - 2 * bird.numberOfCoconuts
-        "노르웨이 파랑 앵무" -> if (bird.isNailed) {
-            0
-        } else {
-            10 + bird.voltage / 10
-        }
-        else -> -1
-    }
+    return Bird(bird).airSpeedVelocity()
 }
--- src/main/kotlin/domain/ch10/Bird.kt
+++ src/main/kotlin/domain/ch10/Bird.kt
@@ -6,4 +6,38 @@ class Bird(
         val numberOfCoconuts: Int,
         val voltage: Int,
         val isNailed: Boolean,
-)
+) {
+    constructor(bird: Bird) : this(bird.name, bird.type, bird.numberOfCoconuts, bird.voltage, bird.isNailed) {
+    }
+
+    fun plumage(): String {
+        return when (this.type) {
+            "유럽 제비" -> "보통이다"
+            "아프리카 제비" -> if (this.numberOfCoconuts > 2) {
+                "지쳤다"
+            } else {
+                "보통이다"
+            }
+            "노르웨이 파랑 앵무" -> if (this.voltage > 100) {
+                "그을렸다"
+            } else {
+                "예쁘다"
+            }
+            else -> "알 수 없다."
+        }
+    }
+
+
+    fun airSpeedVelocity(): Int {
+        return when (this.type) {
+            "유럽 제비" -> 35
+            "아프리카 제비" -> 35 - 2 * this.numberOfCoconuts
+            "노르웨이 파랑 앵무" -> if (this.isNailed) {
+                0
+            } else {
+                10 + this.voltage / 10
+            }
+            else -> -1
+        }
+    }
+}
